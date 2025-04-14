f0b3a8945f8ef9afc6039828de5a88172f054eec
Jon-Muniay <jmuniayhij@educacion.navarra.es>
2025-04-14 10:10:19 +0200

    clase CuentaTest creada, y haciendo el test

---
+++ src/test/java/org/example/CuentaTest.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void getNumero() {
+    }
+
+    @Test
+    void getSaldo() {
+    }
+
+    @Test
+    void setNumero() {
+    }
+
+    @Test
+    void setSaldo() {
+    }
+
+    @Test
+    void ingresarDinero() {
+    }
+
+    @Test
+    void extraerDinero() {
+    }
+
+    @Test
+    void mostrarCuenta() {
+    }
+    @Test
+    void getSaldo1() {
+        Cuenta cuentaPedro = new Cuenta("ES20",100);
+        float saldo = cuentaPedro.getSaldo();
+        assertEquals(100,saldo);
+    }
+    @Test
+    void getSaldo2() {
+        Cuenta cuentaPedro = new Cuenta("ES20",100);
+        float saldo = cuentaPedro.getSaldo();
+        assertEquals(50,saldo);
+    }
+
+}
